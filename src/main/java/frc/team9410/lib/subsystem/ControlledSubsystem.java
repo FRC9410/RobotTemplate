8ea927d2bd0f84a11af7264857bdb1a2dc180d98
Chad <chad.lee@live.com>
2024-06-02 15:25:13 -0500

    Added PID Controller to Controlled Subsystem

--- src/main/java/frc/team9410/lib/subsystem/ControlledSubsystem.java
+++ src/main/java/frc/team9410/lib/subsystem/ControlledSubsystem.java
@@ -1,9 +1,24 @@
 package frc.team9410.lib.subsystem;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.AbsoluteEncoder;
 
 public class ControlledSubsystem extends BaseSubsystem {
+    private SparkPIDController pidController;
+    private AbsoluteEncoder absoluteEncoder;
+    private RelativeEncoder relativeEncoder;
 
     public ControlledSubsystem() {
         super();
+    }
+
+    public void addAbosultePIDController(SparkPIDController pidController, AbsoluteEncoder encoder) {
+        this.pidController = pidController;
+        this.absoluteEncoder = encoder;
+    }
+
+    public void addRelativePIDController(SparkPIDController pidController, RelativeEncoder encoder) {
+        this.pidController = pidController;
+        this.relativeEncoder = encoder;
     }
     
     @Override
